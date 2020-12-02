31338a20eb2adbd9fb438e30e7226065a49aea9d
Christian Parker <christianparker512@gmail.com>
2020-12-02 08:24:43 -0600

    Udemy Java Practice Lesson #5

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Cat");
+        System.out.print("Dog");
+        System.out.print("Rabbit");
+
+    }
+}
