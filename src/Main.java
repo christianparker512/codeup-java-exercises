e13613d346cc6f74a3d15ccba2ac474cd224df71
Christian Parker <christianparker512@gmail.com>
2020-12-02 08:29:55 -0600

    Udermy practice lesson #7 formatting escape sequences

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Cat");
-        System.out.print("Dog");
-        System.out.print("Rabbit");
-
+//        System.out.println("Cat\n");
+//        System.out.print("Dog\n");
+//        System.out.print("R\tabb\bit");
+        System.out.print("The Dog said \"Woof\"");
     }
 }
