7be004ea43fa052a93ff7bff8f65c8748b568ef3
Christian Parker <christianparker512@gmail.com>
2020-12-07 15:01:23 -0600

    for loops

--- src/whileLoop.java
+++ src/whileLoop.java
@@ -1,5 +1,9 @@
 public class whileLoop {
     public static void main (String [] args){
+        for (int i= 0; i <10; i ++) {
+            System.out.println("Test");
+        }
+        System.out.println();
         int i = 0;
         while (i  <10) {
             System.out.println("Test");
