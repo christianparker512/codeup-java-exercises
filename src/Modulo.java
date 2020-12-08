af06c35b8918d6bedc14f0b4205e02a1b9815c6f
Christian Parker <christianparker512@gmail.com>
2020-12-08 08:56:14 -0600

    Modulo in Java

---
+++ src/Modulo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Modulo {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(23 %5);
+        System.out.println(15 %5 == 0);
+        System.out.println(15 %2 == 0);
+    }
+
+}
