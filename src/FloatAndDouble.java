48a3118c57d3219bb46b5a5616bd134000b16f87
Christian Parker <christianparker512@gmail.com>
2020-12-07 19:54:19 -0600

    Float and double examples in Java

---
+++ src/FloatAndDouble.java
@@ -0,0 +1,18 @@
+public class FloatAndDouble {
+    public static void main (String [] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float mininum value = "+ myMinFloatValue);
+        System.out.println("Float maximum value = "+ myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+       double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Float mininum value = "+ myMinDoubleValue);
+        System.out.println("Float maximum value = "+ myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = (float)5.25;
+        double myDoubleValue = 5.25d;
+    }
+}
+
