b40edb02fb6fd11bad265dd8a6c87cb44d0d71da
Christian Parker <christianparker512@gmail.com>
2020-12-11 14:32:36 -0600

    more on methods in Java

--- src/Methods.java
+++ src/Methods.java
@@ -17,22 +17,52 @@ public class Methods {
 //            int finalScore = score + (levelCompleted * bonus);
 //            System.out.println("Your final score was " + finalScore);
 //        }
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Christian", highScorePosition);
 
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Lisa", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Quincy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Baxter", highScorePosition);
     }
-    public static int calculateScore( boolean gameOver, int score, int levelCompleted, int bonus){
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+                + highScorePosition + " on the high score table");
+    }
+    public static int calculateHighScorePosition(int playerScore) {
+            if (playerScore > 1000){
+            return 1;
+            } else if (playerScore > 500 && playerScore < 1000) {
+                return 2;
+            } else if(playerScore > 100 && playerScore < 500){
+                    return 3;
+            } else{
+                return 4;
+            }
+    }
+
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 //        boolean gameOver = true;
 //        int score = 800;
 //        int levelCompleted = 5;
 //        int bonus = 100;
-        if (gameOver){
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("you final score was " + finalScore);
             return finalScore;
         }
-            return -1;
-        }
+        return -1;
+//        }
+
     }
+}
 
 
 
