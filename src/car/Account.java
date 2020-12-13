7848b4abd914803b78bd5c1cc9d9ebbe57a884ed
Christian Parker <christianparker512@gmail.com>
2020-12-13 16:53:14 -0600

    Constructor 2

--- src/car/Account.java
+++ src/car/Account.java
@@ -19,6 +19,9 @@ public class Account {
         this.customerPhoneNumber = customerPhoneNumber;
 
     }
+    public Account (String customerName, String customerEmailAddress, String customerPhoneNumber){
+        this("99999", 100.55, customerName, customerEmailAddress, customerPhoneNumber);
+    }
     public void deposit (double depositAmount){
         this.balance += depositAmount;
         System.out.println("Deposit of " + depositAmount + "made. New balance is " + this.balance);
--- src/car/Main.java
+++ src/car/Main.java
@@ -6,9 +6,25 @@ public class Main {
 //        car holden = new car();
 //        porsche.setModel("911");
 //        System.out.println("Model is " + porsche.getModel());
-        Account bobsAccount = new Account ("12345", 2500.00,"Christian Parker", "christianparker512@gmail.com", "512555-1212");
-        bobsAccount.withdrawal(100.0);
-        bobsAccount.deposit(51.0);
-        bobsAccount.withdrawal(100);
+//        Account bobsAccount = new Account ("12345", 2500.00,"Christian Parker", "christianparker512@gmail.com", "512555-1212");
+//        bobsAccount.withdrawal(100.0);
+//        bobsAccount.deposit(51.0);
+//        bobsAccount.withdrawal(100);
+
+        Account chrisAccount = new Account("Chris", "chris@email.com", "12345");
+        System.out.println(chrisAccount.getNumber() + " name " + chrisAccount.getCustomerName());
+        System.out.println("Current balance is " + chrisAccount.getBalance());
+        chrisAccount.withdrawal(100.55);
+
+
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipPerson person3 = new VipPerson("chris", 100.00);
+        System.out.println(person3.getName());
+        System.out.println(person3.getEmailAddress());
     }
 }
