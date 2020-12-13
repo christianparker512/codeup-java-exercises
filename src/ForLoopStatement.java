77e0f91458d14eccc4bf8f9a4a82846f7a50e43f
Christian Parker <christianparker512@gmail.com>
2020-12-13 11:57:23 -0600

    Sum 3 and 5 with java

--- src/ForLoopStatement.java
+++ src/ForLoopStatement.java
@@ -1,7 +1,7 @@
 public class ForLoopStatement {
     public static void main(String[] args) {
 //        This will go on forever due to it always being higher than zero
-        for (int number = 100; number> 0; number += 10){
+        for (int number = 100; number< 200; number += 10){
             System.out.println("number = " + number);
         }
     }
