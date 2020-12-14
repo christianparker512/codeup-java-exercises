d2733cfe2f319f551d77ca95c7cb3b99ba3c7056
Christian Parker <christianparker512@gmail.com>
2020-12-14 12:40:38 -0600

    How to make comments in Java

--- src/JavaTutorial.java
+++ src/JavaTutorial.java
@@ -1,7 +1,7 @@
 public class JavaTutorial {
     public static void main (String[] args){
-        System.out.print("Hello World!");
-        System.out.print("A Hello World!");
-        System.out.print("B Hello World!");
+//        TODO Auto-generated method stub
+       System.out.print("A");
+        System.out.print("A");
     }
 }
