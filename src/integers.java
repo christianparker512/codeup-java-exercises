211af4d35c9e64996759ad32f33bfa71eec85428
Christian Parker <christianparker512@gmail.com>
2020-12-14 13:12:08 -0600

    integers, doubles and longs

---
+++ src/integers.java
@@ -0,0 +1,12 @@
+public class integers {
+    public static void main(String[] args) {
+        String a = "Chris Parker";
+        int i = 5;
+       double b = 2.3;
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(a + b);
+        System.out.println(i + b);
+    }
+}
