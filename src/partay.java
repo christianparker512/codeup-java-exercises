670ae70a90bfe5bd23be20364b04cb9d616d390b
Christian Parker <christianparker512@gmail.com>
2020-12-14 12:55:36 -0600

    How to get string input from user in java

---
+++ src/partay.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class partay {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter student 1:");
+        String student1 = scan.nextLine();
+        System.out.println("Enter student 2:");
+        String student2 = scan.nextLine();
+        System.out.println("Enter student 3:");
+        String student3 = scan.nextLine();
+        System.out.println("Enter student 4:");
+        String student4 = scan.nextLine();
+        System.out.println("Enter student 5:");
+        String student5 = scan.nextLine();
+        System.out.println("Enter student 6:");
+        String student6 = scan.nextLine();
+
+
+
+    }
+}
