29149f9de861a8f0f5363a3519b3eeb670b4b5b4
Christian Parker <christianparker512@gmail.com>
2020-12-15 08:31:53 -0600

    modulus fun

---
+++ src/ModulusFun.java
@@ -0,0 +1,9 @@
+public class ModulusFun {
+    public static void main(String[] args) {
+        System.out.println(4 * 2);
+        System.out.println(4 %2);
+        System.out.println(3 %2);
+        System.out.println(15 %4);
+        System.out.println(1.5 % .5);
+    }
+}
