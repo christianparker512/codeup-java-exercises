cddbdee839c6ae803c3ce94600a2590e732ba8ed
Christian Parker <christianparker512@gmail.com>
2020-12-16 10:10:17 -0600

    collections and array list in java

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ArrayLists {
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> randomNumbers = new ArrayList<>(Arrays.asList(1,2,3));
 
+//        System.out.println(randomNumbers.get(5));
+        System.out.println(randomNumbers);
         // Add elements to an ArrayList. The element type is the determined by the Object type within the angle brackets <>
         numbers.add(10);
         numbers.add(12);
@@ -18,14 +23,18 @@ public class ArrayLists {
 
         // return an element at the specified index
         System.out.println(numbers.get(1));
-
+//        System.out.println(numbers.get(5));  results in out of bounds exceptions
         // returns index position of found element, -1 if not found.
         System.out.println(numbers.indexOf(13));
         System.out.println(numbers.indexOf(20));
 
         // TODO: Add a new Integer to the numbers ArrayList at index 1.
+        numbers.add(11);
+        System.out.println(numbers);
+
 
         ArrayList<String> roasts = new ArrayList<>();
+        ArrayList<String> emptyList = new ArrayList<>();
         roasts.add("light");
         roasts.add("medium");
         roasts.add("medium");
@@ -42,11 +51,24 @@ public class ArrayLists {
 
         // returns the last occurrence of a given value
         // TODO: Find the last "medium" roast in the list.
+        System.out.println(roasts.lastIndexOf("medium"));
 
         // TODO: How could we check if the roasts list is empty?
-        // TODO: Remove one medium roast from the list. If there are duplicate strings in the list, which one is removed first?
+        System.out.println(emptyList.isEmpty());
+//        // TODO: Remove one medium roast from the list. If there are duplicate strings in the list, which one is removed first?
+//        The first one that it comes across. It reads left to right.
+
+        System.out.println(roasts);
+        System.out.println(roasts.remove("espresso"));
         // TODO: Remove the element at index 4. Note the return value here vs. the previous remove method.
         // BONUS TODO: How can we get the list of roasts in alphabetical order?
+        System.out.println(roasts.remove(4));
+        System.out.println(roasts);
 
+//        How to put the array in alphabetical order
+//        Collections.reverseOrder(roasts);
+        Collections.sort(roasts);
+
+        System.out.println(roasts);
     }
 }
