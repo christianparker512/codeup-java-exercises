19b24e590ce68d32da52b872a813f56579f0e2da
Christian Parker <christianparker512@gmail.com>
2020-12-17 17:00:53 -0600

    Grocery list and grades

--- src/Superman/vehicle.java
+++ src/Superman/vehicle.java
@@ -1,5 +1,27 @@
 package Superman;
 
-public class vehicle {
-    super
+
+
+class Vehicle {
+    int maxSpeed = 120;
+    public void vroom(){
+        System.out.println("Vroom Vroom");
+    }
+
 }
+
+class Car extends Vehicle{
+    int maxSpeed = 100;
+        public void vroom() {
+            System.out.println("Skuurrt!");
+        }
+
+//    public void display() {
+//        System.out.println(super.maxSpeed);
+//    }
+//    int maxSpeed = 100;
+
+}
+
+
+
