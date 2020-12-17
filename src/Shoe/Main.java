9e9d846263d9d33d60cc0cce23efb72092fd30a6
Christian Parker <christianparker512@gmail.com>
2020-12-16 21:09:34 -0600

    subclass and superclass

---
+++ src/Shoe/Main.java
@@ -0,0 +1,14 @@
+package Shoe;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Shoe w = new Shoe("Nike", 12);
+    Shoe r = new Shoe("New Balance", 12);
+        System.out.println(w.brand);
+        System.out.println(w.size);
+        System.out.println(r.brand);
+        System.out.println(r.size);
+
+    }
+}
---
+++ src/Shoe/Running.java
@@ -0,0 +1,10 @@
+package Shoe;
+
+public class Running extends Shoe{
+    public final double weight;
+
+    Running(double weight, String brand, double size){
+        super(brand,size);
+        this.weight = weight;
+    }
+}
