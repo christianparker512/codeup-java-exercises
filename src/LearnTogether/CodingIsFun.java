de19c1f023a76d66825420fbe9801afeedebb18a
Christian Parker <christianparker512@gmail.com>
2020-12-21 17:09:15 -0600

    implementing and instance of

---
+++ src/LearnTogether/CodingIsFun.java
@@ -0,0 +1,11 @@
+package LearnTogether;
+
+public class CodingIsFun {
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "Chris");
+        Student s2 = new Student (2, "Lisa");
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
--- src/javaPractice/Main.java
+++ src/javaPractice/Main.java
@@ -8,7 +8,7 @@ public class Main {
         dog.eat();
         dog.walk();
         dog.run();
-        fish.swim();
+//        fish.swim();
 
     }
 
