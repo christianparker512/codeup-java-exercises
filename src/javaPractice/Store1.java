ff4a0b668518713431f4373b50b4b25e39c1a834
Christian Parker <christianparker512@gmail.com>
2020-12-27 14:10:50 -0600

    This is an example of an instance

---
+++ src/javaPractice/Store1.java
@@ -0,0 +1,20 @@
+package javaPractice;
+
+public class Store1 {
+
+    // new method: constructor!
+    public Store1() {
+        System.out.println("I am inside the constructor method.");
+    }
+
+    // main method is where we create instances!
+    public static void main(String[] args) {
+        System.out.println("Start of the main method.");
+
+        // create the instance below
+        Store1 lemonadeStand = new Store1();
+
+        // print the instance below
+        System.out.println(lemonadeStand);
+    }
+}
