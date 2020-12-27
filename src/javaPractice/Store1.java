c62f7a1301c765a26f21b7017ff43e490f82ea60
Christian Parker <christianparker512@gmail.com>
2020-12-27 14:24:59 -0600

    My car example of classes, objects and contructor parameters

--- src/javaPractice/Store1.java
+++ src/javaPractice/Store1.java
@@ -1,10 +1,12 @@
 package javaPractice;
 
 public class Store1 {
+    String productType;
 
     // new method: constructor!
-    public Store1() {
+    public Store1(String product) {
         System.out.println("I am inside the constructor method.");
+       productType = product;
     }
 
     // main method is where we create instances!
@@ -12,9 +14,9 @@ public class Store1 {
         System.out.println("Start of the main method.");
 
         // create the instance below
-        Store1 lemonadeStand = new Store1();
+//        Store1 lemonadeStand = new Store1();
 
         // print the instance below
-        System.out.println(lemonadeStand);
+//        System.out.println(lemonadeStand);
     }
 }
