0d8dbcf460387462371a51076d1c165956661399
Christian Parker <christianparker512@gmail.com>
2020-12-28 16:42:58 -0600

    Adding parameters to the methods in Java - codeacademy

--- src/javaPractice/Store4.java
+++ src/javaPractice/Store4.java
@@ -9,16 +9,21 @@ public class Store4 {
 //    constructor method
     public Store4 (String product){
         productType = product;
+
     }
     //advertise method
     public void advertise(){
         System.out.println("Come spend some money!!");
         System.out.println("Selling " + productType + "!");
     }
+    public void product(){
+        System.out.println("video games");
+    }
 
     public static void main(String[] args) {
         Store4 lemonadeStand = new Store4 ("Lemonade");
         Store4 videoGameStore = new Store4("video");
+        videoGameStore.product();
         lemonadeStand.advertise();
         videoGameStore.advertise();
 
