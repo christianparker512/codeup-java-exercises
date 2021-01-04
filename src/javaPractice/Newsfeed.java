5db6e3ccb29bce4f3fe8eaf0e963ce1cdbb4c99b
Christian Parker <christianparker512@gmail.com>
2021-01-04 07:50:12 -0600

    Arrays review from Codeacademy

--- src/javaPractice/Newsfeed.java
+++ src/javaPractice/Newsfeed.java
@@ -4,25 +4,30 @@ import java.util.Arrays;
 
 public class Newsfeed {
 
-    String[] topics = {"Opinion", "Tech", "Science", "Health"};
-    int[] views = {0, 0, 0, 0};
+    String[] topics;
 
-    public Newsfeed(){
-
-    }
-
-    public String[] getTopics(){
-        return topics;
-    }
-
-    public int getNumTopics(){
-        return topics.length;
+    public Newsfeed(String[] initialTopics){
+        topics = initialTopics;
     }
 
     public static void main(String[] args){
-        Newsfeed sampleFeed = new Newsfeed();
+        Newsfeed feed;
+        if(args[0].equals("Robot")){
+            //topics for a Robot feed:
+            String[] robotTopics = {"Oil", "Parts", "Algorithms", "Love"};
+            feed = new Newsfeed(robotTopics);
+        }
+        else if(args[0].equals("Human")){
+            //topics for a Human feed:
+            String[] humanTopics = {"Politics", "Science", "Sports", "Love"};
+            feed = new Newsfeed(humanTopics);
+        }
+        else{
+            String[] genericTopics = {"Opinion", "Tech", "Science", "Health"};
+            feed = new Newsfeed(genericTopics);
+        }
 
-        System.out.println("The number of topics is "+ sampleFeed.getNumTopics());
-
+        System.out.println("The topics in this feed are:");
+        System.out.println(Arrays.toString(feed.topics));
     }
 }
